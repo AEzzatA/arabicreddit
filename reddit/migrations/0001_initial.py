54a987ddff68523d4d74c463a3f86ee6d681230d
Ahmed Ezzat <AEzzatA@gmail.com>
2015-01-26 01:41:13 +0200

    Added basic models

---
+++ reddit/migrations/0001_initial.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=140)),
+                ('slug', models.SlugField()),
+                ('text', models.TextField(blank=True)),
+                ('url', models.URLField(blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Subreddit',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('admin', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='subreddit',
+            field=models.ForeignKey(to='reddit.Subreddit'),
+            preserve_default=True,
+        ),
+    ]
--- reddit/models.py
+++ reddit/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Subreddit(models.Model):
+	name = models.CharField(max_length=50)
+	admin = models.ForeignKey(User)
+
+class Post(models.Model):
+	creator = models.ForeignKey(User)
+	subreddit = models.ForeignKey(Subreddit)
+	title = models.CharField(max_length=140)
+	nsfw = models.BooleanField(default=False)
+	slug = models.SlugField()
+	text = models.TextField(blank=True)
+	url = models.URLField(blank=True)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+
